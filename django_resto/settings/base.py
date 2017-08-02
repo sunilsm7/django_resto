3efbbc9e318ba7e342bef995b25886325e2f435b
Sunil <sunil9766@gmail.com>
2017-08-02 17:47:30 +0530

    updated production

--- django_resto/settings/base.py
+++ django_resto/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9a4p8c^)wz!_45qop7m-o%c-febos&3q$-)nkgyu(wte6_kl7^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','foodies-spot.herokuapp.com']
 
 
 # EMAIL_HOST = 'smtp.email-host-provider-domain.com'
